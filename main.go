d0f0ade0be5961fb23a03b405647b498a0d5c98c
kusabashira <kusabashira227@gmail.com>
2016-01-15 23:17:22 +0900

    Implement command as rough

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+
+	"github.com/yuya-takeyama/argf"
+)
+
+var (
+	name  = "ncat"
+	usage = fmt.Sprintf("usage: %s N [FILE]...", name)
+)
+
+func printErr(err error) {
+	fmt.Fprintf(os.Stderr, "%s: %s\n", name, err)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(2)
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		printErr(err)
+		os.Exit(2)
+	}
+
+	r, err := argf.From(os.Args[2:])
+	if err != nil {
+		printErr(err)
+		os.Exit(2)
+	}
+
+	src, err := ioutil.ReadAll(r)
+	if err != nil {
+		printErr(err)
+		os.Exit(1)
+	}
+
+	switch {
+	case n < 0:
+		for {
+			os.Stdout.Write(src)
+		}
+	default:
+		for i := 0; i < n; i++ {
+			os.Stdout.Write(src)
+		}
+	}
+}
